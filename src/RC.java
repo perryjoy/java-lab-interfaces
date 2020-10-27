3dd94c2f4072cd3c7c70a076242281289003cf94
nineleven <tim-sania2008@mail.ru>
2020-10-28 00:08:50 +0300

    Better retcodes

--- src/RC.java
+++ src/RC.java
@@ -3,13 +3,12 @@ package ru.spbstu.pipeline;
 public enum RC {
 
 	CODE_SUCCESS, // ошибки не произошло
-	CODE_INVALID_ARGUMENT, // аргумент командной строки невалиден
+	CODE_INVALID_ARGUMENT, // передан невалидный аргумент
 	CODE_FAILED_TO_READ, // невозможно прочитать из потока
 	CODE_FAILED_TO_WRITE, // невозможно записать в поток
-	CODE_BAD_FORMATTING, // ошибка в грамматике конфига
-	CODE_INPUT_FILE_OPEN, // невозможно открыть поток на чтение
-	CODE_OUTPUT_FILE_OPEN, // невозможно открыть поток на запись
-	CODE_INVALID_SYNTAX_PARAMETER, // ошибка в конфиге - не найден параметр или найден лишний
-	CODE_INVALID_SEMANTIC_PARAMETER, // ошибка в конфиге - в параметре ошибка
+	CODE_INVALID_INPUT_STREAM, // невозможно открыть поток на чтение
+	CODE_INVALID_OUTPUT_STREAM, // невозможно открыть поток на запись
+	CODE_CONFIG_GRAMMAR_ERROR, // ошибка в грамматике конфига
+	CODE_CONFIG_SEMANTIC_ERROR, // семантическая ошибка в конфиге
 	CODE_FAILED_PIPELINE_CONSTRUCTION // при конструировании конвейера произошла ошибка
 }
